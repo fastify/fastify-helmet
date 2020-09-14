94ab6e0622cdfd539b97915eda697f02b0545123
Maksim Sinik <maksim@sinik.it>
2020-09-14 15:22:10 +0200

    Removes esModuleInterop from tsd and adds star import for helmet (#95)
    * Removes esModuleInterop from tsd and adds star import for helmet
    
    * Uses FastifyPluginCallback exported from fastify top level exports

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
-import { FastifyPlugin } from "fastify";
-import helmet from "helmet";
+import { FastifyPluginCallback } from "fastify";
+import * as helmet from "helmet";
 
 type FastifyHelmetOptions = Parameters<typeof helmet>[0];
 
-export const fastifyHelmet: FastifyPlugin<NonNullable<FastifyHelmetOptions>>;
+export const fastifyHelmet: FastifyPluginCallback<NonNullable<FastifyHelmetOptions>>;
 
 export default fastifyHelmet;
