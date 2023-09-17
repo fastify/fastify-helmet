52a72462d9f8021bcf7f881a9ff1956c29e5a561
KrainovSD <106754881+KrainovSD@users.noreply.github.com>
2023-09-17 19:46:21 +0300

    fix nestjs fastify platform warn (#231)
    * fix nestjs fastify platform warn
    
    * Apply suggestions from code review
    
    Co-authored-by: Gürgün Dayıoğlu <gurgun.dayioglu@icloud.com>
    
    * fix tests
    
    ---------
    
    Co-authored-by: Uzlopak <aras.abbasi@googlemail.com>
    Co-authored-by: Gürgün Dayıoğlu <gurgun.dayioglu@icloud.com>

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ async function fastifyHelmet (fastify, options) {
 
   fastify.addHook('onRequest', async (request, reply) => {
     /* istanbul ignore next */
-    const { helmet: routeOptions } = request.routeOptions.config || request.context.config
+    const { helmet: routeOptions } = request.routeOptions?.config ||
+    request.routeConfig
 
     if (typeof routeOptions !== 'undefined') {
       const { enableCSPNonces: enableRouteCSPNonces, skipRoute, ...helmetRouteConfiguration } = routeOptions
@@ -52,7 +53,8 @@ async function fastifyHelmet (fastify, options) {
 
   fastify.addHook('onRequest', (request, reply, next) => {
     /* istanbul ignore next */
-    const { helmet: routeOptions } = request.routeOptions.config || request.context.config
+    const { helmet: routeOptions } = request.routeOptions?.config ||
+    request.routeConfig
 
     if (typeof routeOptions !== 'undefined') {
       const { enableCSPNonces: enableRouteCSPNonces, skipRoute, ...helmetRouteConfiguration } = routeOptions
